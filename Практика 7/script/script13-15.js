b680517e69b848587e72ca6dd0a46049b07a6606
NaVo1319 <70800751+NaVo1319@users.noreply.github.com>
2020-12-11 13:54:35 +0300

    Задания 7.13-7.15

---
+++ Практика 7/script/script13-15.js
@@ -0,0 +1,33 @@
+let tooltipElem;
+//Задание 13
+    document.onmouseover = function(event) {
+      let target = event.target;
+      let tooltipHtml = target.dataset.tooltip;
+      if (!tooltipHtml) return;
+      tooltipElem = document.createElement('div');
+      tooltipElem.className = 'tooltip';
+      tooltipElem.innerHTML = tooltipHtml;
+      document.body.append(tooltipElem);
+      let coords = target.getBoundingClientRect();
+      let left = coords.left + (target.offsetWidth - tooltipElem.offsetWidth) / 2;
+      if (left < 0) left = 0; 
+      let top = coords.top - tooltipElem.offsetHeight - 5;
+      if (top < 0) { 
+        top = coords.top + target.offsetHeight + 5;
+      }
+      tooltipElem.style.left = left + 'px';
+      tooltipElem.style.top = top + 'px';
+    };
+    document.onmouseout = function(e) {
+      if (tooltipElem) {
+        tooltipElem.remove();
+        tooltipElem = null;
+      }
+    };
+    document.onclick=function(event){
+        var a=event.target;
+        var elem=document.getElementById("img");
+        if(a.id>0 && a.id<5){
+            elem.setAttribute("src",a.getAttribute("src"));
+        }
+    }
